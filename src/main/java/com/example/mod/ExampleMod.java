575d5cb0cb4e12d767a1973e27f7c7960c4ac2cc
dxcvvxd <antonsmunk@gmail.com>
2026-04-21 10:32:44 +0300

    Initial commit: Fabric mod project with GitHub Actions

---
+++ src/main/java/com/example/mod/ExampleMod.java
@@ -0,0 +1,10 @@
+package com.example.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
